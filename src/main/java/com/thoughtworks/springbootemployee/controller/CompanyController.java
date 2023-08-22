415e6a79a54cfe7698bfdbb5f588ead467696971
Daphne Balane <balanedaphne20@gmail.com>
2023-08-22 21:04:00 +0800

    refactor: moved the RequestMapping annotation above the  CompanyController class

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@RequestMapping(path = "/companies")
 @RestController
 public class CompanyController {
     @Autowired
@@ -12,38 +14,38 @@ public class CompanyController {
     @Autowired
     private CompanyRepository companyRepository;
 
-    @GetMapping("/companies")
+    @GetMapping
     public List<Company> listAllCompanies() {
         return companyRepository.listAllCompanies();
     }
 
-    @GetMapping("/companies/{id}")
+    @GetMapping("/{id}")
     public Company findCompanyById(@PathVariable Long id) {
         return companyRepository.findCompanyById(id);
     }
 
-    @GetMapping("/companies/{id}/employees")
+    @GetMapping("/{id}/employees")
     public List<Employee> listEmployeesByCompanyId(@PathVariable Long id) {
         return employeeRepository.listEmployeesByCompanyId(id);
     }
 
-    @GetMapping("/companies")
+    @GetMapping(params = {"pageNumber", "pageSize"})
     public List<Company> listCompaniesByPage(@RequestParam Long pageNumber, @RequestParam Long pageSize) {
         return companyRepository.listCompaniesByPage(pageNumber, pageSize);
     }
 
-    @PostMapping("/companies")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Company addCompany(@RequestBody Company company) {
         return companyRepository.addCompany(company);
     }
 
-    @PutMapping("/companies/{id}")
+    @PutMapping("/{id}")
     public Company updateCompany(@PathVariable Long id, @RequestBody Company company) {
         return companyRepository.updateCompany(id, company);
     }
 
-    @DeleteMapping("/companies/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCompany(@PathVariable Long id) {
         companyRepository.deleteCompany(id);
