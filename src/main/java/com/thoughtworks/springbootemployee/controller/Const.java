4d01a87b8152a593719dc7278544aa845ca7a6c3
Daphne Balane <balanedaphne20@gmail.com>
2023-08-22 22:31:22 +0800

    feat: added Const class

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/Const.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.springbootemployee.controller;
+
+public class Const {
+    public static final long START_ID_MINUS_ONE = 0L;
+    public static final int ID_INCREMENT = 1;
+
+    public static final int PAGE_ONE = 1;
+}
