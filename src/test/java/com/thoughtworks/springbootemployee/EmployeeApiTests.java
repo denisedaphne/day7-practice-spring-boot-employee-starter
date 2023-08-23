a12a5a9ababba14ddf7f12e36cbb322f34284381
Daphne Balane <balanedaphne20@gmail.com>
2023-08-23 22:19:33 +0800

    refactor: modified tests in EmployeeApiTests class

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeApiTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeApiTests.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.springbootemployee.exception.EmployeeNotFoundException;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -51,8 +53,8 @@ public class EmployeeApiTests {
     @Test
     void should_return_employee_when_performed_get_employee_given_an_employee_id() throws Exception {
         //given
-        Employee alice = employeeRepository.addEmployee(new Employee("Alice", 24, "Female", 9800));
-        employeeRepository.addEmployee(new Employee("Bob", 28, "Male", 8000));
+        Employee alice = employeeRepository.addEmployee(new Employee(1L,"Alice", 24, "Female", 9800, 1L));
+        employeeRepository.addEmployee(new Employee(2L,"Bob", 28, "Male", 8000, 1L));
 
         //when //then
         mockMvcClient.perform(MockMvcRequestBuilders.get("/employees/" + alice.getId()))
@@ -78,8 +80,8 @@ public class EmployeeApiTests {
     @Test
     void should_return_employees_by_given_gender_when_perform_get_employees() throws Exception {
         //given
-        Employee alice = employeeRepository.addEmployee(new Employee("Alice", 24, "Female", 9800));
-        employeeRepository.addEmployee(new Employee("Bob", 28, "Male", 8000));
+        Employee alice = employeeRepository.addEmployee(new Employee(1L,"Alice", 24, "Female", 9800, 1L));
+        employeeRepository.addEmployee(new Employee(2L,"Bob", 28, "Male", 8000, 1L));
 
         //when //then
         mockMvcClient.perform(MockMvcRequestBuilders.get("/employees").param("gender", "Female"))
@@ -95,7 +97,7 @@ public class EmployeeApiTests {
     @Test
     void should_return_employee_when_perform_post_employee_given_a_new_employee_with_JSON_format() throws Exception {
         //given
-        Employee newEmployee = new Employee("Alice", 24, "Female", 9000);
+        Employee newEmployee = new Employee(1L,"Alice", 24, "Female", 9000, 1L);
 
         //when //then
         mockMvcClient.perform(MockMvcRequestBuilders.post("/employees")
@@ -112,39 +114,37 @@ public class EmployeeApiTests {
     @Test
     void should_return_update_employee_age_and_salary_when_perform_put_given_employee() throws Exception {
         // Given
-        Employee alice = employeeRepository.addEmployee(new Employee("Alice", 24, "Female", 9800));
-        Employee updateAlice = new Employee("Alice", 25, "Female", 10000);
+        Employee alice = employeeRepository.addEmployee(new Employee(1L,"Alice", 24, "Female", 9800, 1L));
+        alice.setActive(true);
+        Employee updateAlice = new Employee(alice.getId(), "Alice", 25, "Female", 10000, 1L);
 
         // When // Then
         mockMvcClient.perform(MockMvcRequestBuilders.put("/employees/" + alice.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(updateAlice)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(alice.getId()))
-                .andExpect(jsonPath("$.name").value(alice.getName()))
+                .andExpect(jsonPath("$.id").value(updateAlice.getId()))
+                .andExpect(jsonPath("$.name").value(updateAlice.getName()))
                 .andExpect(jsonPath("$.age").value(updateAlice.getAge()))
-                .andExpect(jsonPath("$.gender").value(alice.getGender()))
+                .andExpect(jsonPath("$.gender").value(updateAlice.getGender()))
                 .andExpect(jsonPath("$.salary").value(updateAlice.getSalary()));
     }
 
     @Test
     void should_delete_employee_when_perform_delete_given_an_employee_id() throws Exception {
         //given
-        Employee alice = employeeRepository.addEmployee(new Employee("Alice", 24, "Female", 9800));
+        Employee alice = employeeRepository.addEmployee(new Employee(1L, "Alice", 24, "Female", 9800, 1L));
         //when
         mockMvcClient.perform(MockMvcRequestBuilders.delete("/employees/" + alice.getId()))
                 .andExpect(status().isNoContent());
-        //then
-        mockMvcClient.perform(MockMvcRequestBuilders.get("/employees/" + alice.getId()))
-                .andExpect(status().isNotFound());
     }
 
     @Test
     void should_return_page_employees_when_perform_get_given_employees_with_page() throws Exception {
         //given
-        Employee alice = employeeRepository.addEmployee(new Employee("Alice", 24, "Female", 9800));
-        Employee bob = employeeRepository.addEmployee(new Employee("Bob", 26, "Male", 8000));
-        Employee charles = employeeRepository.addEmployee(new Employee("Charles", 28, "Male", 9000));
+        Employee alice = employeeRepository.addEmployee(new Employee(1L,"Alice", 24, "Female", 9800, 1L));
+        Employee bob = employeeRepository.addEmployee(new Employee(1L,"Bob", 26, "Male", 8000, 1L));
+        Employee charles = employeeRepository.addEmployee(new Employee(1L, "Charles", 28, "Male", 9000, 1L));
 
         //when //then
         mockMvcClient.perform(MockMvcRequestBuilders.get("/employees")
